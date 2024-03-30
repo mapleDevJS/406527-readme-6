7f1af2676391fca6ab3760bab83b41ed9f4de0e3
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2024-03-30 16:23:31 -0400

    Mark PostUserRepository and PostUserFactory as Injectable
    The `PostUserRepository` and `PostUserFactory` classes in the `post-user-module` are now decorated with the `@Injectable()` decorator. This makes them available for injection throughout the application. In addition, `PostUserRepository` and `PostUserFactory` are now registered in the module providers for `PostUserModule` and `PostUserRepository` is exported.

--- project/libs/user/post-user/src/post-user-module/post-user.repository.ts
+++ project/libs/user/post-user/src/post-user-module/post-user.repository.ts
@@ -2,7 +2,9 @@ import { BaseMemoryRepository } from '@project/data-access';
 
 import { PostUserEntity } from './post-user.entity';
 import { PostUserFactory } from './post-user.factory';
+import {Injectable} from "@nestjs/common";
 
+@Injectable()
 export class PostUserRepository extends BaseMemoryRepository<PostUserEntity> {
   constructor(entityFactory: PostUserFactory) {
     super(entityFactory);
