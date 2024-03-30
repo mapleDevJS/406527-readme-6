d8deba49b73740477cedd0374fe0b416f933f647
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2024-03-30 16:48:18 -0400

    Add user verification and getUserId functionality
    Supported login functionality by introducing verifyUser method in AuthenticationService which uses findByEmail method to validate user credentials, throwing specific exceptions for incorrect inputs. Also, added getUser method enabling retrieval of a specific user by their id. Updated relevant constants to supplement these changes.

--- project/libs/user/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 import { PostUserRepository, PostUserEntity } from '@project/post-user';
 
 import { CreateUserDto } from '../dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.constant';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant';
+import { LoginUserDto } from '../dto/login-user.dto';
 @Injectable()
 export class AuthenticationService {
   constructor(
@@ -17,8 +18,7 @@ export class AuthenticationService {
       email, firstname, lastname, avatar, passwordHash: ''
     };
 
-    const existUser = await this.postUserRepository
-      .findByEmail(email);
+    const existUser = await this.postUserRepository.findByEmail(email);
 
     if (existUser) {
       throw new ConflictException(AUTH_USER_EXISTS);
@@ -29,5 +29,24 @@ export class AuthenticationService {
 
     return this.postUserRepository
       .save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = this.postUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!await existUser.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    return this.postUserRepository.findById(id);
   }
 }
