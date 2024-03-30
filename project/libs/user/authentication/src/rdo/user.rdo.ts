21e69b1d597e2004853a30c908995526991114cb
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2024-03-30 16:49:43 -0400

    Add UserRdo and LoggedUserRdo classes
    Created new classes UserRdo and LoggedUserRdo in the authentication library for user and logged-in user data objects respectively. These classes include properties such as id, email, and other user-specific details. These are fundamental for managing user identity information across the authentication module.

---
+++ project/libs/user/authentication/src/rdo/user.rdo.ts
@@ -0,0 +1,18 @@
+import { Expose } from 'class-transformer';
+
+export class UserRdo {
+  @Expose()
+  public id: string;
+
+  @Expose()
+  public avatar: string;
+
+  @Expose()
+  public email: string;
+
+  @Expose()
+  public firstname: string;
+
+  @Expose()
+  public lastname: string;
+}
