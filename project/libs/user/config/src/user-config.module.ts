dc4c5dced2f683d77394c49360ddb78d07aa6465
Alexey <alexey.ivanov.private@gmail.com>
2024-04-06 19:49:38 -0400

    Add MongoDB configuration and validation
    MongoDB configuration file, `mongo.config.ts`, has been added with details like host, port, username, password etc. User input validations have also been put in place using Joi. The new configuration file is now included in the `user-config.module.ts`.

--- project/libs/user/config/src/user-config.module.ts
+++ project/libs/user/config/src/user-config.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import applicationConfig from './configurations/app.config';
+import mongoConfig from './configurations/mongo.config';
 
 const ENV_USERS_FILE_PATH = 'apps/user/user.env';
 
@@ -10,7 +11,7 @@ const ENV_USERS_FILE_PATH = 'apps/user/user.env';
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [applicationConfig],
+      load: [applicationConfig, mongoConfig],
       envFilePath: ENV_USERS_FILE_PATH
     }),
   ]
