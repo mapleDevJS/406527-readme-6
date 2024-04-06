9aa10ce3ffef6a8aa4788d9e2245fcc81c7ea8e8
Alexey <alexey.ivanov.private@gmail.com>
2024-04-06 19:50:59 -0400

    Add UserConfigModule to app.module
    UserConfigModule has been added to the app.module import section. This change ensures that the necessary user configuration settings are imported and ready for use within the application.

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import {PostUserModule} from "@project/post-user";
-import {AuthenticationModule} from "@project/authentication";
+import { AuthenticationModule } from '@project/authentication';
+import { UserConfigModule } from '@project/config';
 
 @Module({
   imports: [
     PostUserModule,
-    AuthenticationModule
+    AuthenticationModule,
+    UserConfigModule
   ],
   controllers: [],
   providers: [],
