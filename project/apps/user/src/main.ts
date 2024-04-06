e7064e4910c2c78f458f9a569af9a5ddbc22fb3c
Alexey <alexey.ivanov.private@gmail.com>
2024-04-06 19:20:13 -0400

    Add Swagger API documentation to 'User' service
    Swagger has been configured with corresponding API tags and response descriptions to enhance API documentation. This includes the 'LoginUserDto', 'CreateUserDto', 'LoggedUserRdo', and 'UserRdo' class annotations. Generated documentation will now provide clear and concise API responses, alongside updated user-related constant messages.

--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -1,10 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,7 +8,17 @@ const PORT = process.env.PORT || 3000;
 const GLOBAL_PREFIX = 'api';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «User» service')
+    .setDescription('User service API')
+    .setVersion('1.0')
+    .build();
+
   app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
 
   await app.listen(PORT);
   Logger.log(`🚀 Application is running on: http://localhost:${PORT}/${GLOBAL_PREFIX}`);
--- project/libs/user/authentication/src/dto/create-user.dto.ts
+++ project/libs/user/authentication/src/dto/create-user.dto.ts
@@ -1,7 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique email address',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Keks',
+  })
   public firstname: string;
+
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Ivanov'
+  })
   public lastname: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: 'Password'
+  })
   public password: string;
+
+  @ApiProperty({
+    description: 'User avatar',
+    example: '/avatar.png'
+  })
   public avatar?: string;
 }
