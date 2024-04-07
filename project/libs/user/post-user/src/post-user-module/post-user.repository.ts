86b4dd4a1b7c7d810e2140f8fa044ad014e2f99a
Alexey <alexey.ivanov.private@gmail.com>
2024-04-06 20:38:59 -0400

    Switch PostUserRepository from memory to MongoDB
    The PostUserRepository now extends from BaseMongoRepository instead of BaseMemoryRepository. This shift brings PostUserEntity storage from an in-memory data structure to a MongoDB collection. Changes include altering the constructor and findByEmail method to match this new database strategy.

--- project/libs/user/post-user/src/post-user-module/post-user.repository.ts
+++ project/libs/user/post-user/src/post-user-module/post-user.repository.ts
@@ -1,19 +1,24 @@
-import { BaseMemoryRepository } from '@project/data-access';
+import {Injectable} from "@nestjs/common";
+import {Model} from "mongoose";
+
+import { BaseMongoRepository } from '@project/data-access';
 
 import { PostUserEntity } from './post-user.entity';
 import { PostUserFactory } from './post-user.factory';
-import {Injectable} from "@nestjs/common";
+import { PostUserModel } from './post-user.model';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class PostUserRepository extends BaseMemoryRepository<PostUserEntity> {
-  constructor(entityFactory: PostUserFactory) {
-    super(entityFactory);
+export class PostUserRepository extends BaseMongoRepository<PostUserEntity, PostUserModel> {
+  constructor(
+    entityFactory: PostUserFactory,
+    @InjectModel(PostUserModel.name) blogUserModel: Model<PostUserModel>
+  ) {
+    super(entityFactory, blogUserModel);
   }
 
   public async findByEmail(email: string): Promise<PostUserEntity | null> {
-    const entities = Array.from(this.entities.values());
-    const user = entities.find((entity) => entity.email === email);
-
-    return user ? this.entityFactory.create(user) : null;
+    const document = await this.model.findOne({email}).exec();
+    return this.createEntityFromDocument(document);
   }
 }
