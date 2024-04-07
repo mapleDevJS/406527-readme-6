466072c094ba4c3cdcaaf86beb29e284b840263c
Alexey <alexey.ivanov.private@gmail.com>
2024-04-06 20:35:52 -0400

    Add MongooseModule to post-user.module
    The MongooseModule has been imported and added to the imports array in the post-user module. This allows for the usage of Mongoose, a powerful tool for data modeling in applications reliant on MongoDB, specifically for the PostUserModel.

--- project/libs/user/post-user/src/post-user-module/post-user.module.ts
+++ project/libs/user/post-user/src/post-user-module/post-user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import {PostUserRepository} from "./post-user.repository";
 import {PostUserFactory} from "./post-user.factory";
+import {PostUserModel, PostUserSchema} from "./post-user.model";
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: PostUserModel.name, schema: PostUserSchema }
+  ])],
   providers: [PostUserRepository, PostUserFactory],
   exports: [PostUserRepository],
 })
