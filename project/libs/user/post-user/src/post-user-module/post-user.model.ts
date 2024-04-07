c0cf003514b301dc61009528f7b8182917e6c02e
Alexey <alexey.ivanov.private@gmail.com>
2024-04-06 20:28:52 -0400

    Add PostUserModel schema to the application
    A new class, PostUserModel, has been added to the 'post-user-model' module. This includes a schema definition for a 'users' collection in our database through the @nestjs/mongoose module. The PostUserModel contains properties for User: avatar, email, firstname, lastname, and passwordHash.

---
+++ project/libs/user/post-user/src/post-user-module/post-user.model.ts
@@ -0,0 +1,35 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AuthUser } from '@project/core';
+
+@Schema({
+  collection: 'users',
+  timestamps: true,
+})
+export class PostUserModel extends Document implements AuthUser {
+  @Prop()
+  public avatar: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  public email: string;
+
+  @Prop({
+    required: true,
+  })
+  public firstname: string;
+
+  @Prop({
+    required: true,
+  })
+  public lastname: string;
+
+  @Prop({
+    required: true,
+  })
+  public passwordHash: string;
+}
+
+export const PostUserSchema = SchemaFactory.createForClass(PostUserModel);
