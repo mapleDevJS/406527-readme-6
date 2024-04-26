94b1ae8902b5f91155f6f1848d3e7debfe1badc7
Alexey Ivanov <alexeyivanov@fb.com>
2024-04-26 17:39:03 -0400

    Add PrismaClientModule and PrismaClientService to post models

--- project/libs/post/models/src/index.ts
+++ project/libs/post/models/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/post-models';
+export { PrismaClientModule } from './prisma-client-module/prisma-client.module';
+export { PrismaClientService } from './prisma-client-module/prisma-client.service';
